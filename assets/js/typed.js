6e2c3e0c4e2e5ca8608d9e13fbe098fbee13d5ad
Nigel2421 <nigel2421@yahoo.com>
2024-10-30 21:33:00 +0300

    dishi1

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -10,8 +10,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // Initialize the second typed instance
   new Typed("#typed-2", {
     stringsElement: "#typed-strings-2",  // Element containing the second set of strings
-    typeSpeed: 5,                      // Speed of typing
-    backSpeed: 3,                      // Speed of backspacing
+    typeSpeed: 3,                      // Speed of typing
+    backSpeed: 1,                      // Speed of backspacing
     loop: true                          // Loop the typing animation
   });
 });
